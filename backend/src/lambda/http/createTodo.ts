421bb86f8d153f061d7e1bbfe730a8af3a5de306
Edward Brunson <eddiebrunson@gmail.com>
2020-07-25 04:53:41 -0400

    Update files to test createTodo

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,40 +1,52 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
-import { createTodo } from '../../businessLogic/todos'
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
-import { createLogger } from '../../utils/logger'
-/* import { getUserId } from "../../helpers/authHelper"; */
-import { getToken } from '../../helpers/authHelper'
+import * as AWS  from 'aws-sdk'
 
-const logger = createLogger('createTodoHandler');
+const docClient = new AWS.DynamoDB.DocumentClient()
+import { getUserId } from '../utils' 
+
+
+const todosTable = process.env.TODOS_TABLE
+import uuid from 'uuid'
+
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  logger.info('new todo', event)
-  console.log("Event:", event)
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
-  /*const authHeader = event.headers['Authorization'] */ 
-  /*const userId = getUserId(authHeader)*/
-  /*logger.info(`create todo item for user ${userId} with data ${newTodo}`);*/
-  // TODO: Implement creating a new TODO item
-console.log('Processing event: ', event)
-logger.info('Processing event: ', event)
+  const id = getUserId(event)
 
-const jwtToken: string = getToken(event.headers.Authorization)
-  /*const todoItems = await createTodo(jwtToken)*/
+  console.log("Incoming Todo",newTodo)
+
+  const todoId = uuid.v4()
+  const createdAt = new Date().toISOString()
+
+  const theTodo = {
+    userId: id,
+    todoId,
+    createdAt,
+    done: false,
+    attatchmentUrl: '',
+    ...newTodo
+  }
+
+  console.log("The created TODO is ", theTodo)
+
+  await docClient.put({
+    TableName: todosTable,
+    Item: theTodo
+  }).promise()
+
+  console.log("The result from DynamDB")
   
-
-const newItem = await createTodo(newTodo, jwtToken)
-console.log(newItem)
-return {
-  statusCode: 201,
-  headers: {
-    'Access-Control-Allow-Origin':'*',
-    'Access-Control-Allow-Credentials': true
-  },
-  body: JSON.stringify({
-    items: newItem
-  })
-}
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({item: theTodo})
+  }
+  
 }
