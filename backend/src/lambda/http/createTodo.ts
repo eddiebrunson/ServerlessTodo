79d1b9c53cd2fa41efb8a0c72ce648fdcfd8e93f
Edward Brunson <eddiebrunson@gmail.com>
2020-07-25 20:45:14 -0400

    Update to get past 400 error

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,52 +1,26 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import { createTodo } from '../../businessLogic/todos'
+import { createLogger } from '../../utils/logger';
+import { CreateTodoRequest } from '../../requests/CreateTodoRequest';
 
-import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
-import * as AWS  from 'aws-sdk'
-
-const docClient = new AWS.DynamoDB.DocumentClient()
-import { getUserId } from '../utils' 
-
-
-const todosTable = process.env.TODOS_TABLE
-import uuid from 'uuid'
-
+const logger = createLogger('createTodoHandler');
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const newTodo: CreateTodoRequest = JSON.parse(event.body)
-  const id = getUserId(event)
 
-  console.log("Incoming Todo",newTodo)
+  logger.info('new todo item', event);
 
-  const todoId = uuid.v4()
-  const createdAt = new Date().toISOString()
+  const newTodo: CreateTodoRequest = JSON.parse(event.body);
+  const authorization = event.headers.Authorization;
+  const split = authorization.split(' ');
+  const jwtToken = split[1];
 
-  const theTodo = {
-    userId: id,
-    todoId,
-    createdAt,
-    done: false,
-    attatchmentUrl: '',
-    ...newTodo
-  }
-
-  console.log("The created TODO is ", theTodo)
-
-  await docClient.put({
-    TableName: todosTable,
-    Item: theTodo
-  }).promise()
-
-  console.log("The result from DynamDB")
-  
+  const newItem = await createTodo(newTodo, jwtToken);
   return {
-    statusCode: 201,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true
-    },
-    body: JSON.stringify({item: theTodo})
-  }
-  
-}
+      statusCode: 201,
+      body: JSON.stringify({
+          newItem,
+      }),
+  };
+};
