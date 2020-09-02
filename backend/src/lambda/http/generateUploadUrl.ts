5ba1970aa4adc0fd4efbff995a11f3cfc695aaf9
Edward Brunson <eddiebrunson@gmail.com>
2020-09-02 15:47:15 -0400

    Test upload

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,24 +1,18 @@
 import 'source-map-support/register'
 import { createLogger } from '../../utils/logger'
-import * as AWSXRay from 'aws-xray-sdk';
+/*import * as AWSXRay from 'aws-xray-sdk';*/
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 /*import * as AWS from 'aws-sdk';*/
-/*import { setTodoAttachmentUrl } from '../../businessLogic/todos'*/
-/*import { setTodoAttachmentUrl } from '../../dataLogic/dataAccess'*/
-import * as AWS from 'aws-sdk';
+import { setTodoAttachmentUrl } from '../../businessLogic/todos'
 
 const logger = createLogger('generateUploadUrl')
-const XAWS = AWSXRay.captureAWS(AWS);
+/*const XAWS = AWSXRay.captureAWS(AWS);*/
 const bucketName = process.env.S3_BUCKET
-const urlExpiration = parseInt(process.env.SIGNED_URL_EXPIRATION);
+/*const urlExpiration = parseInt(process.env.SIGNED_URL_EXPIRATION);*/
 /*let options: AWS.S3.Types.ClientConfiguration = { signatureVersion: 'v4', };*/
 
 /*const s3bucket = new XAWS.S3(options);*/
-
-const s3 = new XAWS.S3({
-  signatureVersion: 'v4'
-})
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   logger.info('Processing GenerateUploadUrl', event)
@@ -26,24 +20,26 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
   /*const todoId = event.pathParameters.todoId;*/
   /*
   const todoId = event.pathParameters.todoId;
+  */
+  const todoId = event.pathParameters.todoId
+  const authorization = event.headers.Authorization;
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
   
   setTodoAttachmentUrl(
     todoId,
     `https://${bucketName}.s3.amazonaws.com/${todoId}.png`,
   );
 
-  const url = s3bucket.getSignedUrl('putObject', {
+  /*const url = s3bucket.getSignedUrl('putObject', {
     Bucket: bucketName,
     Key: `${todoId}.png`,
     Expires: urlExpiration,
   });
   */
-  const todoId = event.pathParameters.todoId
-  /*const authorization = event.headers.Authorization;
-  const split = authorization.split(' ')
-  const jwtToken = split[1]*/
+  
 
- const url = await getUploadUrl(todoId)
+ const url = await setTodoAttachmentUrl(todoId,jwtToken)
 
   return {
     statusCode: 200,
@@ -56,10 +52,3 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
     }),
   };
 };
-function getUploadUrl(todoId: string) {
-  return s3.getSignedUrl('putObject', {
-    Bucket: bucketName,
-    Key: todoId,
-    Expires: urlExpiration
-  })
-}
