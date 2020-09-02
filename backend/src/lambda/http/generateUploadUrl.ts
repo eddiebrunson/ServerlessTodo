b6811d72ef9fec0a182c60713e7ddb6a3289a2fe
Edward Brunson <eddiebrunson@gmail.com>
2020-09-02 16:12:05 -0400

    Revert change

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,46 +1,21 @@
 import 'source-map-support/register'
 import { createLogger } from '../../utils/logger'
-import * as AWSXRay from 'aws-xray-sdk';
-
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
-import * as AWS from 'aws-sdk';
-/*import { setTodoAttachmentUrl } from '../../businessLogic/todos'*/
+import { setTodoAttachmentUrl } from '../../businessLogic/todos'
 
 const logger = createLogger('generateUploadUrl')
-const XAWS = AWSXRay.captureAWS(AWS);
-const bucketName = process.env.S3_BUCKET
-const urlExpiration = parseInt(process.env.SIGNED_URL_EXPIRATION);
-let options: AWS.S3.Types.ClientConfiguration = { signatureVersion: 'v4', };
-
-const s3bucket = new XAWS.S3(options);
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   logger.info('Processing GenerateUploadUrl', event)
   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  /*const todoId = event.pathParameters.todoId;*/
-  /*
-  const todoId = event.pathParameters.todoId;
-  
-  setTodoAttachmentUrl(
-    todoId,
-    `https://${bucketName}.s3.amazonaws.com/${todoId}.png`,
-  );
-*/
+
   const todoId = event.pathParameters.todoId
   const authorization = event.headers.Authorization;
   const split = authorization.split(' ')
   const jwtToken = split[1]
 
-  const url = s3bucket.getSignedUrl('putObject', jwtToken, {
-    Bucket: bucketName,
-    Key: {todoId},
-    Expires: urlExpiration,
-  });
-  
-  
-/*
  const url = await setTodoAttachmentUrl(todoId,jwtToken)
-*/
+
   return {
     statusCode: 200,
     headers: {
