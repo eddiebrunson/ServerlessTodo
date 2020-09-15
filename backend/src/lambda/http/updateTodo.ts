116b3deafd04e1b8e9e8ef0459273c8d6081aa53
Edward Brunson <eddiebrunson@gmail.com>
2020-09-15 03:27:04 -0400

    Remove commented code

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,15 +1,9 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
-
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 import { createLogger } from '../../utils/logger'
-/*import { getUserId } from '../../helpers/authHelper'*/
-
 import { updateTodo } from '../../businessLogic/todos'
 import { getToken } from '../../helpers/authHelper'
-
-
 
 const logger = createLogger('todos')
 
@@ -19,8 +13,6 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
 
   // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-
-  /*const authHeader = event.headers.Authorization*/
   
   const jwtToken: string = getToken(event.headers.Authorization)
 
@@ -35,38 +27,3 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
     body: 'update successful',
 };
 }
-
-/*
-import "source-map-support/register";
-import { updateTodo } from "../../BusinessLogic/todos";
-import {
-  APIGatewayProxyEvent,
-  APIGatewayProxyHandler,
-  APIGatewayProxyResult
-} from "aws-lambda";
- 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent)
-
-: Promise<APIGatewayProxyResult> => {
-
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-
-  const updatedToDo = await updateTodo(event);
-
-  return {
-
-    statusCode: 200,
-
-    headers: {
-
-      "Access-Control-Allow-Origin": "*",
-
-      "Access-Control-Allow-Credentials": true
-
-    },
-
-    body: JSON.stringify({ msg: "T0-do has been updated", updated: updatedToDo })
-
-  };
-};
-*/
